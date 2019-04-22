a017c5df4e03b829f469d0859cad561baf0cf5bc
adrian <adrian-sw@aporter.org>
2019-04-22 01:57:48 -0400

    style has moved off the server

--- rsync.sh
+++ rsync.sh
@@ -2,12 +2,26 @@
 
 set -e #exit on failure
 
+
 base=https://cdn.klokantech.com/mapbox-gl-js/v0.53.0
 
 for filename in mapbox-gl.css mapbox-gl.js; do
 	wget --output-document=$filename "$base/$filename"
 done
 
-wget --output-document=style.json https://tileserver.cyclemap.us/styles/klokantech-basic-cycle/style.json
+
+base='https://tileserver.cyclemap.us/styles/klokantech-basic-cycle'
+
+wget --output-document=style.json "$base/style.json"
+
+sed --in-place --expression="s#$base#https://cyclemap.us/sprite#g" style.json
+
+mkdir --parents sprite
+
+for filename in sprite{,@2x}.{png,json}; do
+	wget --output-document=sprite/$filename "$base/$filename"
+done
+
 
 wget --output-document=js.cookie.min.js https://cdn.jsdelivr.net/npm/js-cookie@2/src/js.cookie.min.js
+
