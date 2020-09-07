037263d1bd39c38888fc57d4094f0c202a735140
adrian <adrian-sw@aporter.org>
2020-09-07 13:41:25 -0400

    change the javascript to show the surface legend at all levels (now, instead, the footpath legend fades in and out)
    (update script now pulls in index.html automatically if we're cloned into the right place)

--- update.sh
+++ update.sh
@@ -12,6 +12,15 @@ done
 
 base='https://tileserver.cyclemap.us/styles/klokantech-basic-cycle'
 
+cp ../../conf/viewer/viewer.tmpl index.html
+sed --in-place \
+	--expression 's#{{public_url}}viewer/##g' \
+	--expression 's#{{public_url}}##g' \
+	--expression 's#styles/klokantech-basic-cycle/##g' \
+	--expression 's#.*tileserver only has this style##' \
+	--expression 's#/sprite@2x#sprite&#' \
+	index.html
+
 wget --output-document=style.json "$base/style.json"
 
 sed --in-place --expression="s#$base#https://cyclemap.us/sprite#g" style.json
