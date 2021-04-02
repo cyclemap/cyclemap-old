2b386da1f17fe5674c7d92be26c90bf3126cf620
adrian <adrian-sw@aporter.org>
2021-04-02 17:01:11 -0400

    maplibre v1.14.0.  letting index.html deviate from tileserver.cyclemap.us again.

--- update.sh
+++ update.sh
@@ -2,43 +2,26 @@
 
 set -e #exit on failure
 
-maplibreVersion=1.13.0-rc.4
-maplibreUrl=https://registry.npmjs.org/maplibre-gl/-/maplibre-gl-$maplibreVersion.tgz
-
-if [ -e 'tokens.conf' ]; then
-	source 'tokens.conf'
-fi
+maplibreVersion=1.14.0
+maplibreUrl=https://github.com/maplibre/maplibre-gl-js/releases/download/v$maplibreVersion/dist.zip
 
 curl \
+	--location \
 	--silent \
 	"$maplibreUrl" |
-tar \
+bsdtar \
 	--extract \
-	--gzip \
-	--strip-components 2 \
-	package/dist/{mapbox-gl.css,mapbox-gl.js}
+	--strip-components 1 \
+	--file -
 
 base='https://tileserver.cyclemap.us/styles/maptiler-cyclemap'
-
-cp --dereference viewer.tmpl index.html
-sed --in-place \
-	--expression 's#{{public_url}}viewer/##g' \
-	--expression 's#{{public_url}}##g' \
-	--expression 's#styles/maptiler-cyclemap/##g' \
-	--expression 's#.*tileserver only has this style##' \
-	--expression 's#/sprite@2x#sprite&#' \
-	--expression "s/mapboxgl.accessToken = null/mapboxgl.accessToken = '$mapboxAccessToken'/" \
-	--expression "s/openrouteAccessToken = null/openrouteAccessToken = '$openrouteAccessToken'/" \
-	index.html
 
 wget --output-document=style.json "$base/style.json"
 
 sed --in-place --expression="s#$base#https://cyclemap.us/sprite#g" style.json
 cp style.json style-normal.json
-cp style.json style-red.json
-sed --in-place --expression='s/hsl(25, 60%, 45%)/red/2' --expression 's/,\["==","true","false"\]//' style-red.json
-cp style.json style-highlight.json
-sed --in-place --expression='s/hsl(120, 60%, 30%)/white/g' --expression='s/hsl(25, 60%, 45%)/white/1' --expression='s/hsl(25, 60%, 45%)/red/' style-highlight.json
+sed --expression='s/hsl(25, 60%, 45%)/red/2' --expression 's/,\["==","true","false"\]//' style.json >style-red.json
+sed --expression='s/hsl(120, 60%, 30%)/white/g' --expression='s/hsl(25, 60%, 45%)/white/1' --expression='s/hsl(25, 60%, 45%)/red/' style.json >style-highlight.json
 
 mkdir --parents sprite
 
