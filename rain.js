577cc132c77f471e6ae64311c552a28bfcc0f199
adrian <adrian-sw@aporter.org>
2023-04-22 21:16:53 -0400

    looks like the meteohydro_sfc_qpe link is broken (http forbidden 403).
    using obs/rfc_qpe instead.

--- rain.js
+++ rain.js
@@ -1,9 +1,13 @@
 
 const TILE_SIZE = 2048;
 
-//layer ids found here:  https://nowcoast.noaa.gov/arcgis/rest/services/nowcoast/analysis_meteohydro_sfc_qpe_time/MapServer/?f=json
-//i assume they're volatile?
-const LAYER_ID_MAP =  {'1': 19, '3': 27};
+//layer ids found here:  curl https://nowcoast.noaa.gov/arcgis/rest/services/nowcoast/analysis_meteohydro_sfc_qpe_time/MapServer/?f=json |jq .
+//also found here     :  curl https://mapservices.weather.noaa.gov/raster/rest/services/obs/rfc_qpe/MapServer/?f=json |jq .
+
+//const RAIN_PATH = 'https://nowcoast.noaa.gov/arcgis/rest/services/nowcoast/analysis_meteohydro_sfc_qpe_time/MapServer/export'; const LAYER_ID_MAP =  {'1d': 19, '3d': 27};
+const RAIN_PATH = 'https://mapservices.weather.noaa.gov/raster/rest/services/obs/rfc_qpe/MapServer/export'; const LAYER_ID_MAP =  {'1d': 25+3, '3d': 37+3, '7d': 53+3};
+
+//5 day and 7 day can be found here:  https://www.wpc.ncep.noaa.gov/qpf/p120i.gif  https://www.wpc.ncep.noaa.gov/qpf/p168i.gif
 
 function setupRain() {
 	map.on('style.load', (e) => {
@@ -15,17 +19,17 @@ function setupRain() {
 }
 
 function addRainButtons() {
-	for(var days in LAYER_ID_MAP) {
-		addRainButton(days);
+	for(var key in LAYER_ID_MAP) {
+		addRainButton(key);
 	}
 }
 
-function addRainButton(days) {
-	var layerId = LAYER_ID_MAP[days];
+function addRainButton(key) {
+	var layerId = LAYER_ID_MAP[key];
 
 	addLayerButton({
-		"id": `rain-history-${days}`,
-		"name": `rain ${days}d`,
+		"id": `rain-history-${key}`,
+		"name": `rain ${key}`,
 		"active": false,
 		"type": "raster",
 		"layout": {"visibility": "visible"},
@@ -35,7 +39,7 @@ function addRainButton(days) {
 			"type": "raster",
 			"tileSize": TILE_SIZE,
 			"tiles" : [
-				`https://nowcoast.noaa.gov/arcgis/rest/services/nowcoast/analysis_meteohydro_sfc_qpe_time/MapServer/export?bbox={bbox-epsg-3857}&size=${TILE_SIZE/4},${TILE_SIZE/4}&dpi=96&format=png8&transparent=true&bboxSR=3857&imageSR=3857&layers=show:${layerId}&f=image`
+				`${RAIN_PATH}?bbox={bbox-epsg-3857}&size=${TILE_SIZE/4},${TILE_SIZE/4}&dpi=96&format=png8&transparent=true&bboxSR=3857&imageSR=3857&layers=show:${layerId}&f=image`
 			],
 		},
 	});
