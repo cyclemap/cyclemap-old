88435fe010988ad2a368af75af2f892e7176b041
adrian <adrian-sw@aporter.org>
2023-06-06 14:08:53 -0400

    handle errors getting files differently.

--- layer.js
+++ layer.js
@@ -207,18 +207,22 @@ function checkAddLayers() {
 function ajaxGet(url, callback) {
 	var xhttp = new XMLHttpRequest();
 	xhttp.addEventListener("load", function() {
+		if(this.status !== 200) {
+			console.error(`did not get ${url}: error ${this.statusText}`);
+			return;
+		}
 		var returnValue = JSON.parse(this.responseText);
 		if(returnValue.error) {
 			if(returnValue.error.message) {
-				alert(`http: ${returnValue.error.message}`);
+				console.error(`did not get ${url}: error ${returnValue.error.message}`);
 				return;
 			}
-			alert(`http: ${returnValue.error}`);
+			console.error(`did not get ${url}: error ${returnValue.error}`);
 			return;
 		}
 		callback(returnValue);
 	});
-	xhttp.addEventListener("error", function() {alert(`http: ${this.statusText}`);});
+	xhttp.addEventListener("error", function() {console.error(`did not get ${url}: error ${this.statusText}`);});
 	xhttp.open("GET", url, true);
 	xhttp.send();
 }
